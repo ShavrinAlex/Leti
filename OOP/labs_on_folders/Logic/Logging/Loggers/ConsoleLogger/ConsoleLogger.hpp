aa29afb2d78ac0897c37501e81404f28b1b36d9e
ShavrinAlex <alex.shavrin200303@gmail.com>
2023-02-15 21:28:05 +0300

    removed unnecessary branch files

--- OOP/labs_on_folders/Logic/Logging/Loggers/ConsoleLogger/ConsoleLogger.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../Logger.hpp"
-
-class ConsoleLogger: public Logger{
-    public:
-        //print log
-        void print(Log* log) override;
-};
--- PR/1/Shavrin_Alexey_lb3/src/get_first_symbol.h
+++
@@ -1,1 +0,0 @@
-char get_first_symbol();
