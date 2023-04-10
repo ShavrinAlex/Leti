f52aadff97026e5188948dd91f0bbd0ca4dcbbaa
ShavrinAlex <alex.shavrin200303@gmail.com>
2023-04-10 14:32:52 +0300

    CaAA lb4 and lb5 done

--- 4_semester/CaAA/Python/lb3/main.py
+++ 4_semester/CaAA/Python/lb3/main.py
@@ -8,6 +8,7 @@ class Node:
     Graph node class that stores the weights of branches to neighboring nodes and a sorted list
     of numbers of child nodes by the cost of the path to them
     """
+
     def __init__(self, node_degree: int) -> None:
         """
         This method initializes the initial parameters of the graph node
@@ -67,39 +68,6 @@ class Graph:
         self.__count_nodes = count_nodes
         self.__nodes = [Node(self.__count_nodes) for _ in range(count_nodes)]
 
-    def get_branch_weight(self, start_node_number: int, end_node_number: int) -> float:
-        """
-        This method returns the weight of the branch between the passed nodes
-        :param start_node_number: the number of the node from which the branch exits
-        :param end_node_number: the number of the node that the branch belongs to
-        :return: the weight of the branch between the start and end nodes
-        """
-
-        return self.__nodes[start_node_number][end_node_number]
-
-    def set_branch_weight(self, start_node_number: int, end_node_number: int, weight: int) -> None:
-        """
-        This method sets the weight of the branch between the passed nodes
-        :param start_node_number: the number of the node from which the branch exits
-        :param end_node_number: the number of the node that the branch belongs to
-        :param weight: the weight of the branch between the start and end nodes
-        :return: None
-        """
-
-        self.__nodes[start_node_number][end_node_number] = weight
-
-    def get_path_weight(self, path: list[int]) -> int:
-        """
-        This method calculates the weight of the transmitted path
-        :param path: list of node numbers
-        :return: the weight of the transmitted path
-        """
-
-        path_weight = 0
-        for i in range(1, len(path)):
-            path_weight += self.__nodes[path[i - 1]][path[i]]
-        return path_weight
-
     def sort(self) -> None:
         """
         This method sorts the lists of child nodes in all nodes of the graph
@@ -117,7 +85,7 @@ class Graph:
 
         return self.__count_nodes
 
-    def __getitem__(self, node_number) -> Node:
+    def __getitem__(self, node_number: int) -> Node:
         """
         This method returns a node object with the passed node number
         :param node_number: the number of the node to get
@@ -129,18 +97,27 @@ class Graph:
 
 class Solver:
     """Solver - a class solving the problem of finding a Hamiltonian path"""
-    def __init__(self, graph) -> None:
+
+    def __init__(self, graph: Graph) -> None:
         """
         This method initializes the initial parameters of the Solver
         :param graph: node graph
         """
 
         self.__graph = graph
-        self.__start_node = 0
+        self.__start_node_number = 0
         self.__best_hamiltonian_cycle = list()
         self.__best_hamiltonian_cycle_weight = inf
 
     def __lower_bound(self, start_node_number: int, current_path: list[int]) -> int:
+        """
+        This method calculates the lower bound
+        (the minimum weight of the spanning tree from vertices not included in the current path)
+        :param start_node_number: the number of the node from which the method will start
+        :param current_path: the current fragment of the Hamiltonian cycle
+        :return: minimum weight of the spanning tree
+        """
+
         min_spanning_tree_weight = 0
         visited_nodes_numbers = []
         visited_nodes_numbers_stack = [start_node_number]
@@ -151,7 +128,7 @@ class Solver:
             current_node_number = visited_nodes_numbers_stack[-1]
 
             for child_node_number in self.__graph[current_node_number].get_children():
-                child_weight = self.__graph.get_branch_weight(current_node_number, child_node_number)
+                child_weight = self.__graph[current_node_number][child_node_number]
 
                 if (child_node_number not in current_path) and (child_weight < min_weight)\
                         and (child_node_number not in visited_nodes_numbers)\
@@ -168,54 +145,42 @@ class Solver:
 
         return min_spanning_tree_weight
 
-    def __find_next_node(self, current_node_number, current_path, visited_branches: list[list[bool]]) -> int:
-        current_node = self.__graph[current_node_number]
+    def __find_next_node_number(self, start_node_number: int, current_path: list[int], visited_branches: list[list[bool]]) -> int:
+        """
+        This method selects the best next node number
+        :param start_node_number: the number of the node from which the method will start
+        :param current_path: the current fragment of the Hamiltonian cycle
+        :param visited_branches: the adjacency matrix in which at the intersection of
+        the i and j node is true if this branch has already been checked
+        :return: the best next node number
+        """
+
+        current_node = self.__graph[start_node_number]
         min_weight = inf
         best_node_number = -1
 
         for child_node_number in current_node.get_children():
-            if (current_node[child_node_number] < min_weight) and (child_node_number not in current_path) and (child_node_number != self.__start_node or len(current_path) == len(self.__graph) - 1):
-                if not visited_branches[current_node_number][child_node_number]:
+            if (current_node[child_node_number] < min_weight) and (child_node_number not in current_path)\
+                    and (child_node_number != self.__start_node_number or len(current_path) == len(self.__graph) - 1):
+                if not visited_branches[start_node_number][child_node_number]:
                     min_weight = current_node[child_node_number]
                     best_node_number = child_node_number
 
         if best_node_number != -1:
-            visited_branches[current_node_number][best_node_number] = True
+            visited_branches[start_node_number][best_node_number] = True
+
         return best_node_number
 
-    """
-    def __greedy_algorithm(self, start_node_number: int, visited_nodes: list[int]) -> list[int]:
-        
-        This method finds a possible minimal Hamiltonian cycle using a greedy algorithm
-        :param start_node_number:
-        :param visited_nodes:
-        :return:
-        
-
-        greedy_hamiltonian_cycle = [start_node_number]
-        while len(greedy_hamiltonian_cycle) + len(visited_nodes) < len(self.__graph):
-            current_node_number = greedy_hamiltonian_cycle[-1]
-
-            for child_node_number in self.__graph[current_node_number].get_children():
-                if (child_node_number in greedy_hamiltonian_cycle) or (child_node_number in visited_nodes):
-                    continue
-                greedy_hamiltonian_cycle.append(child_node_number)
-                break
-
-            if current_node_number == greedy_hamiltonian_cycle[-1]:
-                break
-
-        greedy_hamiltonian_cycle.append(start_node_number)
-        return greedy_hamiltonian_cycle
-    """
     def __branches_and_boundaries_algorithm(self, current_node_number: int, current_path: list[int], current_path_weight: int, visited_branches: list[list[bool]]) -> None:
-        """
-        This method searches for the minimum Hamiltonian cycle by the method of branches and boundaries
+        """This method searches for the minimum Hamiltonian cycle by the method of branches and boundaries
+        :param current_node_number: the number of the node from which the method will start
         :param current_path: list of node numbers in the current Hamiltonian path
         :param current_path_weight: weight of the current Hamiltonian path
+        :param visited_branches: the adjacency matrix in which at the intersection of
+        the i and j node is true if this branch has already been checked
         :return: None
         """
-        #print(self.__best_hamiltonian_cycle_weight, current_path, current_path_weight, self.__lower_bound(current_node_number, current_path))
+
         if current_path_weight > self.__best_hamiltonian_cycle_weight:
             return
 
@@ -229,50 +194,34 @@ class Solver:
             return
 
         while len(current_path) != len(self.__graph):
-            next_node_number = self.__find_next_node(current_node_number, current_path, visited_branches)
+            next_node_number = self.__find_next_node_number(current_node_number, current_path, visited_branches)
 
             if next_node_number == -1:
                 return
 
             new_path = current_path.copy()
-            new_path_weight = current_path_weight + self.__graph.get_branch_weight(current_node_number, next_node_number)
+            new_path_weight = int(current_path_weight + self.__graph[current_node_number][next_node_number])
             new_visited_branches = deepcopy(visited_branches)
             new_path.append(next_node_number)
 
             self.__branches_and_boundaries_algorithm(next_node_number, new_path, new_path_weight, new_visited_branches)
 
-        """"
-        if len(current_path) == len(self.__graph):
-            new_path_weight = current_path_weight + self.__graph.get_branch_weight(current_path[-1], current_path[0])
-            if new_path_weight < self.__best_hamiltonian_cycle_weight:
-                new_path = deepcopy(current_path)
-                new_path.append(current_path[0])
-                self.__best_hamiltonian_cycle = new_path
-                self.__best_hamiltonian_cycle_weight = new_path_weight
-            return
-
-        for node_number in range(len(self.__graph)):
-            if node_number in current_path:
-                continue
-            current_path.append(node_number)
-            current_path_weight += self.__graph.get_branch_weight(current_path[-2], current_path[-1])
-            self.__branches_and_boundaries_algorithm(current_path, current_path_weight)
-            current_path.pop()
+    def find_hamiltonian_cycle(self) -> (list[int], int):
         """
-    def find_hamiltonian_cycle(self) -> list[int]:
-        visited_branches = []
+        This method is a wrapper over the recursive method of branches and bounds
+        :return: Tuple from the list of cycle nodes and path weights
+        """
+
+        visited_branches = [[False for _ in range(len(self.__graph))] for _ in range(len(self.__graph))]
         for first_node_number in range(len(self.__graph)):
-            visited_branches.append([])
             for second_node_number in range(len(self.__graph)):
-                if self.__graph.get_branch_weight(first_node_number, second_node_number) == inf:
+                if self.__graph[first_node_number][second_node_number] == inf:
                     visited_branches[first_node_number].append(True)
-                else:
-                    visited_branches[first_node_number].append(False)
 
-        self.__branches_and_boundaries_algorithm(self.__start_node, [], 0, visited_branches)
-        self.__best_hamiltonian_cycle.insert(0, self.__start_node)
+        self.__branches_and_boundaries_algorithm(self.__start_node_number, [], 0, visited_branches)
+        self.__best_hamiltonian_cycle.insert(0, self.__start_node_number)
 
-        return self.__best_hamiltonian_cycle
+        return (self.__best_hamiltonian_cycle, self.__best_hamiltonian_cycle_weight)
 
 
 class App:
@@ -304,14 +253,14 @@ class App:
         for line in file:
             formatted_line = list(map(lambda x: inf if x.strip() in ["inf", "-"] else int(x), line.split()))
             for i in range(len(formatted_line)):
-                self.__graph.set_branch_weight(node_number, i, formatted_line[i])
+                self.__graph[node_number][i] = formatted_line[i]
             node_number += 1
 
         file.close()
 
     def __format_answer(self) -> None:
         """
-        This method translates the node numbers into the desired form
+        This method translates the node numbers into the desired form and translates work time into milliseconds
         :return: None
         """
 
@@ -328,8 +277,7 @@ class App:
 
         start_time = time()
         self.__graph.sort()
-        self.__answer_hamiltonian_cycle = self.__solver.find_hamiltonian_cycle()
-        self.__answer_hamiltonian_cycle_weight = self.__graph.get_path_weight(self.__answer_hamiltonian_cycle)
+        self.__answer_hamiltonian_cycle, self.__answer_hamiltonian_cycle_weight = self.__solver.find_hamiltonian_cycle()
         self.__work_time = (time() - start_time)
 
     def __str__(self) -> str:
@@ -351,11 +299,16 @@ class App:
 
 
 def main():
-    for i in range(1, 7):
-        print('\n' + "-" * 15 + f"Test {i}" + "-" * 15)
-        answer = App(f"test_{str(i)}.txt")
-        answer.solve()
-        print(answer)
+    """
+    for i in range(1, 6):
+        print('\n' + "-" * 15 + f"pr_{i}" + "-" * 15)
+        app = App(f"pr_{str(i)}")
+        app.solve()
+        print(app)
+    """
+    app = App(f"pr_3")
+    app.solve()
+    print(app)
 
 
 if __name__ == "__main__":
