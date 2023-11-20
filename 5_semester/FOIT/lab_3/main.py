206964513782faef7f929419bdce9e0a2c7e0b5f
ShavrinAlex <alex.shavrin200303@gmail.com>
2023-11-20 11:28:05 +0300

    FOIT/lab_3: done

--- 5_semester/FOIT/lab_3/main.py
+++ 5_semester/FOIT/lab_3/main.py
@@ -35,15 +35,16 @@ def draw_afr(frequency: list, H: list, stop_i: int) -> None:
     plt.plot(frequency[1:stop_i], H[:stop_i-1])
     plt.show()
 
-def calculate_stop_i(N: int, delta_f: float, frequency: list) -> int:
-    """This function calculated stop i"""
+def calculate_stop_i(N: int, delta_f: float) -> tuple:
+    """This function calculated frequency and stop i"""
 
+    frequency = []
     stop_i = 0
     for i in range(N):
         if (2 * np.pi * delta_f * i > 100) and (stop_i == 0):
             stop_i = i + 1
         frequency.append(2 * np.pi * delta_f * i)
-    return stop_i
+    return (frequency, stop_i)
 
 def calculate_h(L1: float, L2: float, C1: float, C2: float, R1: float, R2: float, R3: float, R4: float, j: complex, omega):
     """This function calculated h"""
@@ -74,27 +75,27 @@ def main():
     R4 = 539.354514679385
     DELTA_T = 0.0196349540849362
     N = 8192
-    INPUT_1 = 4
-    INPUT_2 = 9
-    HARMONIC_NUMBER = 2
     FILE_NAME = "19.txt"
+
 
     j = complex(0, 1)
     delta_f = 1 / (DELTA_T * N)
     signal = read_file(FILE_NAME)
 
-    spectre_module = list(map(abs, np.fft.fft(signal)))
-    frequency = []
-    stop_i = calculate_stop_i(N, delta_f, frequency)
+    spectre_module = list(map(abs, np.fft.fft(signal))) # amplitude list
+    frequency, stop_i = calculate_stop_i(N, delta_f) # frequency list
+
     H = [abs(calculate_h(L1, L2, C1, C2, R1, R2, R3, R4, j, omega)) for omega in frequency[1:]]
-    H_ans = abs(calculate_h(L1, L2, C1, C2, R1, R2, R3, R4, j, 10)) # у первой гармоники ω = 10 рад/c
+    # w2 = 20 
+    H_ans = abs(calculate_h(L1, L2, C1, C2, R1, R2, R3, R4, j, 20))
 
     time = [DELTA_T * i for i in range(N)]
     draw_signal(time, signal)
     draw_amplitude(frequency, spectre_module, stop_i)
     draw_afr(frequency, H, stop_i)
 
-    print(H_ans)
+    print(1/H_ans)
+
 
 
 if __name__ == "__main__":
