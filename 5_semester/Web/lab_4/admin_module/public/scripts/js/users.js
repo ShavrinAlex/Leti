dc11654050d704f1a5b88570b728758a120b5248
ShavrinAlex <alex.shavrin200303@gmail.com>
2024-02-16 09:25:17 +0300

    6_semester: created

--- 5_semester/Web/lab_4/admin_module/public/scripts/js/users.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-    $(".user_container").on("click", function () {
-        $.get(`/admin_module/users/${this.id}`)
-        window.location = `/admin_module/users/${this.id}`
-    })
-})
